8bf030d7779f3dce2628b8e6482f495958a4e67f
Nicholas Evans <nevans85.pro@gmail.com>
2017-08-12 15:06:33 -0700

    write player class

--- chess/display.rb
+++ chess/display.rb
@@ -5,6 +5,9 @@ require_relative 'board'
 
 
 class Display
+
+  attr_reader :cursor
+
   def initialize(board)
     @board = board
     @cursor = Cursor.new([0, 0], @board)
@@ -23,16 +26,10 @@ class Display
         print'|'
       end
       print "\n"
+      puts 'Make a move'
+      puts 'Use arrows to move the cursor and select with space or enter.'
     end
     nil
   end
 
-  def select_position
-    selection = nil
-    while selection.nil?
-      render
-      selection = @cursor.get_input
-    end
-    selection
-  end
 end
--- chess/player.rb
+++ chess/player.rb
@@ -1,7 +1,28 @@
+require_relative 'move'
+require_relative 'display'
+
 class Player
   attr_reader :color, :name
-  def initialize(black_or_white, name = 'Player')
+  def initialize(black_or_white, board, name = 'Human_Player')
     @color = black_or_white
+    @board = board
     @name = name
+    @display = Display.new(@board)
+  end
+
+  def move
+    render
+    start_pos = select_position
+    end_pos = select_position
+    Move.new(start_pos, end_pos)
+  end
+
+  def select_position
+    selection = nil
+    while selection.nil?
+      @display.render
+      selection = @display.cursor.get_input
+    end
+    selection
   end
 end
